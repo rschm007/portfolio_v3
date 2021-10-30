ba307658da5252f722c970ceeadcf619d70d31cb
rschm007 <rschm007@gmail.com>
2021-10-30 00:29:15 -0700

    added rocks

--- src/components/Animations/Campfire/index.js
+++ src/components/Animations/Campfire/index.js
@@ -1,32 +1,42 @@
 const Campfire = () => {
   return (
-    <div class="campfire">
-      <div class="fire">
-        <div class="left">
-          <div class="main_fire"></div>
-          <div class="particle_fire"></div>
+    <div className="campfire">
+      <div className="fire">
+        <div className="left">
+          <div className="main_fire"></div>
+          <div className="particle_fire"></div>
         </div>
-        <div class="main">
-          <div class="main_fire"></div>
-          <div class="particle_fire"></div>
+        <div className="main">
+          <div className="main_fire"></div>
+          <div className="particle_fire"></div>
         </div>
-        <div class="right">
-          <div class="main_fire"></div>
-          <div class="particle_fire"></div>
+        <div className="right">
+          <div className="main_fire"></div>
+          <div className="particle_fire"></div>
         </div>
-        <div class="bottom">
-          <div class="_main"></div>
+        <div className="bottom">
+          <div className="_main"></div>
         </div>
-        <div class="glow" />
+        <div className="glow" />
       </div>
-      <div class="logs">
-        <div class="log" />
-        <div class="log" />
-        <div class="log" />
-        <div class="log" />
-        <div class="log" />
-        <div class="log" />
-        <div class="log" />
+      <div className="logs">
+        <div className="log" />
+        <div className="log" />
+        <div className="log" />
+        <div className="log" />
+        <div className="log" />
+        <div className="log" />
+        <div className="log" />
+      </div>
+      <div className="rocks">
+        <div className="rock_big_1" />
+        <div className="rock_big_2" />
+        <div className="rock_big_3" />
+        <div className="rock_big_4" />
+        <div className="rock_big_5" />
+        <div className="rock_small_1">
+          <div className="rock" />
+        </div>
       </div>
     </div>
   );
