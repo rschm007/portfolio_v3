b3706aee7e1701a4bb7d719a196593b41ad4db64
Robert Schmahl <rschm007@gmail.com>
2022-02-23 15:40:22 -0800

    form complete with validations

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -6,25 +6,32 @@ import Stars from "../../components/Animations/Stars";
 import Campfire from "../../components/Animations/Campfire";
 import { motion } from "framer-motion/dist/framer-motion";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 import { init, sendForm } from "@emailjs/browser";
-init(process.env.EMAILJS_USERID);
+init("user_eYHjyB5qJUnnw935hh5vz");
+
+// using react-hook-form https://react-hook-form.com
+// using emailjs https://dashboard.emailjs.com
 
 const Contact = () => {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
+
+  const form = document.querySelector("#contact_form");
+
   const onSubmit = (data) =>
-    sendForm(
-      process.env.EMAILJS_SERVICEID,
-      "template_xhc183s",
-      "#contact_form"
-    ).then(
+    sendForm("service_qqdv6l8", "template_f2u2wb2", "#contact_form").then(
       (res) => {
         console.log("SUCCESS!", res.status, res.text);
+        form.reset();
+        setFormSubmitted(true);
       },
       (err) => {
         console.log("ERROR", err);
@@ -43,37 +50,45 @@ const Contact = () => {
           <Header tag="h1" content="Contact" class="header_contact__hero">
             <Subtitle
               tag="p"
-              content="You can write me a message if you want."
+              content={
+                formSubmitted
+                  ? "Thanks, talk to you soon!"
+                  : "You can write me a message if you want."
+              }
               class="contact"
             />
-            {errors.name && <span>This field is required</span>}
-            <form id="contact_form" onSubmit={handleSubmit(onSubmit)}>
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                {...register("name", { required: true, maxLength: 30 })}
-              />
-              {errors.email && <span>This field is required</span>}
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                {...register("email", {
-                  required: true,
-                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                })}
-              />
-              {errors.message && <span>This field is required</span>}
-              <textarea
-                name="message"
-                placeholder="Message"
-                maxLength={1500}
-                {...register("message", { required: true, maxLength: 1500 })}
-              />
-              <p className="message-chars-left">{messageCharactersLeft}</p>
-              <input type="submit" value="Send" />
-            </form>
+
+            {!formSubmitted && (
+              <form id="contact_form" onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  {...register("name", { required: true, maxLength: 30 })}
+                />
+                {errors.name && <span>This field is required</span>}
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  {...register("email", {
+                    required: true,
+                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  })}
+                />
+                {errors.email && <span>This field is required</span>}
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  maxLength={1500}
+                  {...register("message", { required: true, maxLength: 1500 })}
+                />
+                {errors.message && <span>This field is required</span>}
+
+                <p className="message-chars-left">{messageCharactersLeft}</p>
+                <input type="submit" value="Send" />
+              </form>
+            )}
           </Header>
 
           <motion.div
