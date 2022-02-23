fd024f73e7432951a22f17a114966df1cba23d4f
Robert Schmahl <rschm007@gmail.com>
2022-02-23 15:23:56 -0800

    progress on contact page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import About from "./pages/About";
 import Work from "./pages/Work";
+import Contact from "./pages/Contact";
 import Navigation from "./components/Navigation";
+
 import { React } from "react";
-// import Contact from "./pages/Contact";
 import { AnimatePresence } from "framer-motion/dist/framer-motion";
 
 const App = () => {
@@ -20,7 +21,7 @@ const App = () => {
         <Route exact path="/home" component={Main} />
         <Route exact path="/about" component={About} />
         <Route exact path="/work" component={Work} />
-        {/* <Route path="/contact" component={Contact} /> */}
+        <Route path="/contact" component={Contact} />
       </Router>
     </AnimatePresence>
   );
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,101 @@
-const Contact = () => {
-    return (
+import Header from "../../components/Header";
+import Subtitle from "../../components/Subtitle";
+import Wrapper from "../../components/Wrapper";
+import LinkButton from "../../components/LinkButton";
+import Stars from "../../components/Animations/Stars";
+import Campfire from "../../components/Animations/Campfire";
+import { motion } from "framer-motion/dist/framer-motion";
+import { useForm } from "react-hook-form";
 
+import { init, sendForm } from "@emailjs/browser";
+init(process.env.EMAILJS_USERID);
+
+const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) =>
+    sendForm(
+      process.env.EMAILJS_SERVICEID,
+      "template_xhc183s",
+      "#contact_form"
+    ).then(
+      (res) => {
+        console.log("SUCCESS!", res.status, res.text);
+      },
+      (err) => {
+        console.log("ERROR", err);
+      }
     );
+
+  const message = watch("message") || "";
+  const messageCharactersLeft = 1500 - message.length;
+
+  return (
+    <Wrapper class="background">
+      <Stars />
+
+      <Wrapper class="background_1">
+        <Wrapper class="contact_container">
+          <Header tag="h1" content="Contact" class="header_contact__hero">
+            <Subtitle
+              tag="p"
+              content="You can write me a message if you want."
+              class="contact"
+            />
+            {errors.name && <span>This field is required</span>}
+            <form id="contact_form" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                {...register("name", { required: true, maxLength: 30 })}
+              />
+              {errors.email && <span>This field is required</span>}
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.message && <span>This field is required</span>}
+              <textarea
+                name="message"
+                placeholder="Message"
+                maxLength={1500}
+                {...register("message", { required: true, maxLength: 1500 })}
+              />
+              <p className="message-chars-left">{messageCharactersLeft}</p>
+              <input type="submit" value="Send" />
+            </form>
+          </Header>
+
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+          >
+            {" "}
+            <Subtitle
+              tag="p"
+              content="Or find me online."
+              class="contact_subtitle__links"
+            />
+          </motion.div>
+        </Wrapper>
+
+        <Wrapper class="campfire_container">
+          <Campfire />
+        </Wrapper>
+      </Wrapper>
+    </Wrapper>
+  );
 };
 
 export default Contact;
