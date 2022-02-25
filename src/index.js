146e5a341095355d83dd842a01fe1ed8a397c01d
rschm007 <rschm007@gmail.com>
2022-02-25 13:31:20 -0800

    hashrouter to preserve page after refreshing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 // Opt into Concurrent Mode
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
